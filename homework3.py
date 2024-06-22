468afca26a633b901ebdc34f9c26229bfe855df7
Андрей Черкашин <Cherkashinandrey@mail.ru>
2024-06-22 23:23:19 +0700

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,9 @@
+#Практическая работа по уроку "Динамическая типизация"
+name="Andrey"
+print("Name: ",name)
+age=41
+print("Age: ",age)
+age=age+1
+print("Age New: ",age)
+is_student =True
+print("Is student: ",is_student)
