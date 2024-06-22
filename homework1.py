243e0c23cf3a6c1decd0f8f96392e7ca97570cec
Андрей Черкашин <Cherkashinandrey@mail.ru>
2024-06-22 23:40:13 +0700

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,12 @@
+#Практическое задание по уроку "Строки и индексация строк"
+example='123456789'
+print(example[0])
+print(example[-1])
+print(example[len(example)//2:])
+print(example[::-1])
+print(example[1::2])
+#Практическое задание по работе в Pycharm - "Переменные"
+HomeTask=12
+time=1.5
+Name='Python'
+print('Курс: Python, всего задач:',HomeTask,', затрачено часов: ',time,', среднее время выполнения ',(time/HomeTask))
