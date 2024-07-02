ba10a274442706c5b9fdf37bb5cb890366699d13
Андрей Черкашин <Cherkashinandrey@mail.ru>
2024-07-02 20:07:32 +0700

    Update module_2_4.py

--- module_2_4.py
+++ module_2_4.py
@@ -1,17 +1,20 @@
 #Домашняя работа по уроку "Цикл for. Элементы списка. Полезные функции в цикле"
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 552]
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes =[]
 not_primes=[]
 for index in range(len(numbers)):
-    if numbers[index] == 2:
+    if numbers[index]<=1:
+        continue
+    elif numbers[index] == 2:
         primes.append(numbers[index])
         continue
-    if numbers[index] % 2 == 0:
-        primes.append(numbers[index])
-        continue
-    for N in range(3, int(numbers[index]**0.5)+1, 2):
+    is_prime = True
+    for N in range(2, numbers[index]):
         if numbers[index] % N == 0:
-            primes.append(numbers[index])
-            continue
-        not_primes.append(numbers[index])
-print(primes, not_primes)
+            not_primes.append(numbers[index])
+            is_prime = False
+            break
+    if is_prime:
+        primes.append(numbers[index])
+print("Primes: ", primes)
+print("Not Primes: ", not_primes)
