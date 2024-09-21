f71744016a5fc61774358ca03271acf716d349d7
Андрей Черкашин <Cherkashinandrey@mail.ru>
2024-09-21 20:09:52 +0700

    Update module_6_3.py

--- module_6_3.py
+++ module_6_3.py
@@ -1,28 +1,43 @@
-#Домашнее задание по теме "Множественное наследование"
+# Домашнее задание по теме "Множественное наследование"
 class Horse:
-    x_distance = 0 # Пройденный путь
-    sound = 'Frrr' #Звук лошади
+    def __init__(self):
+        self.x_distance = 0  # Пройденный путь
+        self.sound = 'Frrr'  # Звук лошади
+
     def run(self, dx: int):
         self.x_distance += dx
         return self.x_distance
+
+
 class Eagle:
-    y_distance = 0 #Высота полета
-    sound = 'I train, eat, sleep, and repeat' # Звук орла
+    def __init__(self):
+        self.y_distance = 0  # Высота полета
+        self.sound = 'I train, eat, sleep, and repeat'  # Звук орла
+
     def fly(self, dy: int):
         self.y_distance += dy
         return self.y_distance
+
+
 class Pegasus(Horse, Eagle):
     def __init__(self):
-        self.sound = Horse().sound
-        self.sound = Eagle().sound
+        super().__init__()
+        Horse.__init__(self)
+        Eagle.__init__(self)
+
+    # Eagle.__init__(self, sound)
+
     def move(self, dx, dy):
         self.run(dx)
         self.fly(dy)
+
     def get_pos(self):
         pos_pegaus = (self.x_distance, self.y_distance)
         return pos_pegaus
+
     def voice(self):
         print(self.sound)
+
 
 p1 = Pegasus()
 
