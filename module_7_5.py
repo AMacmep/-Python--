6e0da64d247229dec5c45e807a6e11d904a6417e
Андрей Черкашин <Cherkashinandrey@mail.ru>
2024-10-07 13:37:43 +0700

    Add files via upload

---
+++ module_7_5.py
@@ -0,0 +1,16 @@
+# Домашнее задание по теме "Файлы в операционной системе".
+import os
+import time
+
+directory = '.'
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        filepath = os.path.join(root, file)
+        print(filepath)
+        filetime = os.path.getmtime(filepath)
+        print(filetime)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime())
+        filesize = os.path.getsize(filepath)
+        parent_dir = os.path.dirname(filepath)
+        print(
+            f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
